473d5ba42ce7415fdb7b027deac5ec4b6f967fe3
Shreyansh Singh <shreyansh071299@gmail.com>
2021-05-10 16:55:13 +0530

    added template engine

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import bodyparser from 'body-parser';
+import path from 'path';
 
 const app = express();
 
@@ -9,6 +11,16 @@ dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080;
 //log requests
 app.use(morgan('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set view engine
+
+app.set("view engine", "ejs");
+
+//load assets
+
+// app.use('/css',express.static)
 
 app.get('/', (req,res)=>{
     res.send("hello");
